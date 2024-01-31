de2f3c18ceeaebfbd870ada037020deb7058511d
Hangarden <52573001+Hangarden@users.noreply.github.com>
2024-01-31 21:15:32 +0900

    Title: 특정 수와 근접한 합, Time: 173ms, Memory: 25MB, Status: Passed - CodeTree

--- 240131/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240131/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -1,37 +1,103 @@
-N, S = map(int, input().split())
+import copy
+
+n, target = map(int, input().split())
 
 lst = list(map(int, input().split()))
+max_val = float('inf')
+visited = [False] * n
+def back(lst, n):
+    global max_val
+    if n == 2:
+        max_val = min(max_val, abs(sum(lst) - target) )
+        return
+    
+    for i in range(len(lst)):
+        if not visited[i]:
+            visited[i] = True
+            x = copy.deepcopy(lst[i])
+            lst[i] = 0
+            back(lst, n+1)
+            lst[i] = x
+            visited[i] = False
+
+back(lst, 0)
+
+print(max_val)
+# N, S = map(int, input().split())
+
+# lst = list(map(int, input().split()))
 
 
 
-lst.sort()
+# lst.sort()
 
-def binary_search(target, lst):
-    start = 0
-    end = len(lst) -1
+# def binary_search(target, lst):
+#     start = 0
+#     end = len(lst) -1
 
 
-    while start <= end:
-        mid = (start + end) // 2
-        if lst[mid] == target:
-            return mid
+#     while start <= end:
+#         mid = (start + end) // 2
+#         if lst[mid] == target:
+#             return mid
 
-        else:
-            if lst[mid] >= target:
-                end = mid -1
-            else:
-                start = mid + 1
-    else:
-        return start
+#         else:
+#             if lst[mid] >= target:
+#                 end = mid -1
+#             else:
+#                 start = mid + 1
+#     else:
+#         return start
 
-# print(binary_search(0, len(lst), find_value))
+# # print(binary_search(0, len(lst), find_value))
 
-for i in range(2):
-    sums = sum(lst)
-    find_value = (sums - S)//(2-i)
-    x = binary_search(find_value, lst)
-    if x >= len(lst):
-        x -=1
-    lst.pop(x)
+# for i in range(2):
+#     sums = sum(lst)
+#     find_value = (sums - S)//(2-i)
+#     x = binary_search(find_value, lst)
+#     if x >= len(lst):
+#         x -=1
+#     lst.pop(x)
 
-print(sum(lst) - S)
+# print(sum(lst) - S)
+
+
+
+
+
+
+
+
+
+
+# lst.sort()
+
+# def binary_search(target, lst):
+#     start = 0
+#     end = len(lst) -1
+
+
+#     while start <= end:
+#         mid = (start + end) // 2
+#         if lst[mid] == target:
+#             return mid
+
+#         else:
+#             if lst[mid] >= target:
+#                 end = mid -1
+#             else:
+#                 start = mid + 1
+#     else:
+#         return start
+
+# # print(binary_search(0, len(lst), find_value))
+
+# for i in range(2):
+#     sums = sum(lst)
+#     find_value = (sums - S)//(2-i)
+#     x = binary_search(find_value, lst)
+#     if x >= len(lst):
+#         x -=1
+#     lst.pop(x)
+
+# print(sum(lst) - S)
