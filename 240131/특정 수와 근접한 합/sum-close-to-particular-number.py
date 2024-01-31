382f3b5942d1b599f7b81c1ba2a0615b51aa4984
Hangarden <52573001+Hangarden@users.noreply.github.com>
2024-01-31 17:02:06 +0900

    Title: 특정 수와 근접한 합, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240131/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -0,0 +1,36 @@
+N, S = map(int, input().split())
+
+lst = list(map(int, input().split()))
+
+sums = sum(lst)
+
+lst.sort()
+
+def binary_search(target, lst):
+    start = 0
+    end = len(lst) -1
+
+
+    while start <= end:
+        mid = (start + end) // 2
+        if lst[mid] == target:
+            return mid
+
+        else:
+            if lst[mid] >= target:
+                end = mid -1
+            else:
+                start = mid + 1
+    else:
+        return start
+
+# print(binary_search(0, len(lst), find_value))
+
+for _ in range(2):
+    find_value = (sums - S)//2
+    x = binary_search(find_value, lst)
+    if x >= len(lst):
+        x -=1
+    lst.pop(x)
+
+print(sum(lst) - S)
