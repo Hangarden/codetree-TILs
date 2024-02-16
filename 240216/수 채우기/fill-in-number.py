d854d67ac8f606efc37f76e78d0823349f0ba662
Hangarden <52573001+Hangarden@users.noreply.github.com>
2024-02-16 22:09:13 +0900

    Title: 수 채우기, Time: 142ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240216/수 채우기/fill-in-number.py
+++ 240216/수 채우기/fill-in-number.py
@@ -1,10 +1,9 @@
 n = int(input())
 
-D = [-1] * (n+2)
+D = [-1] * (100001)
 D[2],D[4], D[5] = 1, 2, 1
 
-
-for i in range(6, n+1):
+for i in range(2, n+1):
     if D[i-2] != -1 or D[i-5] != -1:
         if D[i-2] == -1:
             D[i] = D[i-5] + 1
