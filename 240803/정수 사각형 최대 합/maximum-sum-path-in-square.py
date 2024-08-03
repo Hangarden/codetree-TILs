dcd873433ba7831726a9996367ae2216a50821fe
Hangarden <52573001+Hangarden@users.noreply.github.com>
2024-08-03 17:16:20 +0900

    Title: 정수 사각형 최대 합, Time: 5042ms, Memory: 27MB, Status: Time Limit Exceed - CodeTree

--- 240803/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 240803/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -5,29 +5,26 @@ grid = [
     for _ in range(n)
 ]
 
-max_sum = 0
 
 def in_range(x, y):
     return 0 <= x and x < n and 0 <= y and y < n
 
 
-def find_max_sum(x, y, sum):
-    global max_sum
-    
+def find_max_sum(x, y):
     # 도착 지점에 도착하면 최대 합을 갱신해줍니다.
     if x == n - 1 and y == n - 1:
-        max_sum = max(max_sum, sum)
-        return
+        return grid[n - 1][n - 1]
     
     dxs, dys = [1, 0], [0, 1]
     
     # 가능한 모든 방향에 대해 탐색해줍니다.
+    max_sum = 0 # 주어진 숫자의 범위가 1보다 크기 때문에 항상 갱신됨이 보장됩니다.
     for dx, dy in zip(dxs, dys):
         new_x, new_y = x + dx, y + dy
         
         if in_range(new_x, new_y):
-            find_max_sum(new_x, new_y, sum + grid[new_x][new_y])
-                         
+            max_sum = max(max_sum, find_max_sum(new_x, new_y) + grid[x][y])
+            
+    return max_sum
                     
-find_max_sum(0, 0, grid[0][0])
-print(max_sum)
+print(find_max_sum(0, 0))
