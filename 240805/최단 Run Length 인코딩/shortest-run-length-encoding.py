9d617a55f496a30a834c98b184e70a8799b3b40e
Hangarden <52573001+Hangarden@users.noreply.github.com>
2024-08-05 21:10:56 +0900

    Title: 최단 Run Length 인코딩, Time: 152ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240805/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -0,0 +1,36 @@
+from collections import deque
+string = input()
+if string.count(string[0]) == len(string):
+    answer = string[0]+str(len(string[0]))
+else:
+    # print(string)
+    lst = list(string)
+    lst = deque(lst)
+
+    while (lst[0] == lst[-1]):
+        x = lst.pop()
+        lst.appendleft(x)
+
+    # print(list(lst))
+
+    answer = ""
+
+    initial = ""
+    count = 0
+    for i in lst:
+        # print(i)
+        if initial == "":
+            initial = i
+            count += 1
+            continue
+        
+        if i == initial:
+            count +=1
+        else:
+            answer += initial+str(count)
+            initial = i
+            count = 1
+
+    answer += initial + str(count)
+
+    print(len(answer))
