7e127960631f7d34e3f787d3f31a4fac9e579675
Hangarden <52573001+Hangarden@users.noreply.github.com>
2024-08-06 21:21:02 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 311ms, Memory: 29MB, Status: Passed - CodeTree

--- 240806/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
+++ 240806/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
@@ -1,5 +1,10 @@
 K, N = map(int, input().split())
 
-for i in range(1, K+1):
-    for j in range(1,N+1):
-        print(i, j)
+def recursion(lst, x):
+    if x > N:
+        print(*lst)
+        return
+    for i in range(1, K+1):
+        recursion(lst + [i], x+1)
+
+recursion([],1)
