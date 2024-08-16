f46963bfab612ac8767aeb9e88e8f120ed58f56a
Hangarden <52573001+Hangarden@users.noreply.github.com>
2024-08-16 18:21:00 +0900

    Title: 핀볼게임, Time: 5104ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

--- 240816/핀볼게임/pinball-game.py
+++ 240816/핀볼게임/pinball-game.py
@@ -1,3 +1,8 @@
+dx = [0, -1, 0, 1]  # 오른쪽, 위, 왼쪽, 아래
+dy = [1, 0, -1, 0]
+
+memo = {}  # 메모이제이션을 위한 딕셔너리
+
 def check(i,j):
     if j==0 and 0< i < n-1: # 왼쪽 -> 오른쪽
         return [0]
@@ -16,65 +21,56 @@ def check(i,j):
         return [2,3]
     if i ==(n-1) and j == (n-1):
         return [1,2]
+    
+def throw(i, j, direction):
+    if (i, j, direction) in memo:  # 이미 계산된 경우
+        return memo[(i, j, direction)]
 
-def throw(i,j, direction):
     count = 0
-    while (0 <= i <= n-1 and 0 <= j <= n-1):
-        i += dx[direction]
-        j += dy[direction]
+    x, y = i, j
+
+    while 0 <= x < n and 0 <= y < n:
+        x += dx[direction]
+        y += dy[direction]
         count += 1
-        if not (0 <= i <= n-1 and 0 <= j <= n-1):
+
+        if not (0 <= x < n and 0 <= y < n):
+            memo[(i, j, direction)] = count
             return count
 
-        if maps[i][j] == 1:
-            direction = abs(direction-5) % 4
-        
-        if maps[i][j] == 2:
-            direction = abs(direction-3)
+        if maps[x][y] == 1:
+            direction = abs(direction - 5) % 4  # 기존 수식 사용
 
-        
+        if maps[x][y] == 2:
+            direction = abs(direction - 3) % 4  # 기존 수식 사용
+
+    memo[(i, j, direction)] = count
     return count
 
+
 n = int(input())
-
-
 maps = [list(map(int, input().split())) for _ in range(n)]
 
-nums1 = [0, n-1]
-# print(nums1)
-
-
-dx = [0,-1,0,1]
-dy = [1,0,-1,0]
+nums1 = [0, n - 1]
 max_val = -888
 
-
-for i in nums1: # 0 4
-    for j in nums1: # 0 4
-        # print(i,j)
-        direction = check(i,j)
+for i in nums1:  # 상단과 하단 (i가 0 또는 n-1인 경우)
+    for j in nums1:  # 좌측과 우측 (j가 0 또는 n-1인 경우)
+        direction = check(i, j)
         if len(direction) >= 2:
-            
-            # print(throw(i,j, direction[0]), [i,j])
-            # print(throw(i,j, direction[1]), [i,j])
-            max_val = max(max_val, throw(i,j, direction[0]))
-            max_val = max(max_val, throw(i,j, direction[1]))
-    
-for i in nums1: # 0 4 
-    for j in range(1,n-1):# 1 2 3
-        direction = check(i,j)
+            max_val = max(max_val, throw(i, j, direction[0]))
+            max_val = max(max_val, throw(i, j, direction[1]))
 
-        if len(direction) <= 1:
-            # print(throw(i,j, direction[0]), [i,j])
-            max_val = max(max_val, throw(i,j, direction[0]))
+for i in nums1:  # 상단과 하단
+    for j in range(1, n - 1):  # 가운데 열들
+        direction = check(i, j)
+        if len(direction) == 1:
+            max_val = max(max_val, throw(i, j, direction[0]))
 
-for i in range(1, n-1):
-    for j in nums1:
-        direction = check(i,j)
+for i in range(1, n - 1):  # 가운데 행들
+    for j in nums1:  # 좌측과 우측
+        direction = check(i, j)
+        if len(direction) == 1:
+            max_val = max(max_val, throw(i, j, direction[0]))
 
-        if len(direction) <= 1:
-            # print(throw(i,j, direction[0]), [i,j])
-            max_val = max(max_val, throw(i,j, direction[0]))
-
-
-print(max_val+1)
+print(max_val + 1)
