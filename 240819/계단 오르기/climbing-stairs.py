eb0ea0a4b29912ab85e225064eee9fc4ebb86eb2
Hangarden <52573001+Hangarden@users.noreply.github.com>
2024-08-19 20:03:39 +0900

    Title: 계단 오르기, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240819/계단 오르기/climbing-stairs.py
+++ 240819/계단 오르기/climbing-stairs.py
@@ -11,9 +11,10 @@ dp = [0 for _ in range(n+2)]
 
 dp[0], dp[1]  = 1,0
 
-for i in range(n+1):
-    if n >= 3:
-        dp[n] = dp[n-2] + dp[n-3]
+for i in range(2, n+1):
+    if i >= 3:
+        dp[i] = dp[i-2] + dp[i-3]
     else:
-        dp[n] = dp[n-2]
+        dp[i] = dp[i-2]
+# print(dp)
 print(dp[n] % 10007)
